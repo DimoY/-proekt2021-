392fc3f59aedc10df5fe484348bd1f2143f90afc
Liverpoolfan92 <85640417+Liverpoolfan92@users.noreply.github.com>
2021-06-16 23:51:47 +0300

    fixes in menu, but still bugs

--- menu.c
+++ menu.c
@@ -68,6 +68,7 @@ int ok_login(){
 int login(struct user_t* users_t){
     char username[100];
     char pass[100];
+    char option[100];
     
     printf("Username:");
     fgets(username, 100, stdin);
@@ -77,11 +78,10 @@ int login(struct user_t* users_t){
     fgets(pass, 100, stdin);
     pass[strcspn(pass, "\n")] = 0;
     crypt(pass);
-
+//printing first menu after every run 
     if(check_user(users_t,username,pass) == 1){
-        printf("1");
         int action =  ok_login();
-        printf("1");
+
         if(action == 1){
             printf("Deposit sUS");
             //deposit
@@ -97,20 +97,32 @@ int login(struct user_t* users_t){
         else if(action == 4){
             printf("Transaction processing sUS");
            //transaction processing
+           navigator(3);
         }
         else if(action == 5){
-            printf("Log out\n");
+            printf("................\n");
             //Log out
-            //free(everything)
             first_menu();
 
         }
     }
-    else printf("No such account foundet\n Try again or Sign up:");
+    else{ printf("No such account foundet\n Try again or Sign up:\n");
+        label: fgets(option, 100, stdin);
+        if(strstr(option,"Try again")){
+            login(users_t);
+        }
+        else if(strstr(option, "Sign up")){
+            signup(users_t);
+        }
+        else{
+            printf("Invalide action. Please Try again or Sign up:\n");
+            goto label;
+        }
+    }
 
   return 0;
 }
-//ne raboti
+
 int signup(struct user_t* users_t){
     char username[100];
     char pass[100];
@@ -122,7 +134,9 @@ int signup(struct user_t* users_t){
     pass[strcspn(pass, "\n")] = 0;
     crypt(pass);   
     add_user(users_t,username,pass);
+    //
     //save_user(users_t);
+    //add_smetka
     return 0;
 }
 
@@ -142,10 +156,15 @@ void first_menu_navigator(int action, struct user_t *users_t){
 }
 
 void navigator(int action){
+    int i = 0;
     struct user_t* users_t = load_users();
     while(action != 3){
         action = first_menu();
         first_menu_navigator(action,&users_t);
+        while(i < 20){
+            printf("\n");
+            i++;
+        }
     }
 return; 
 }
